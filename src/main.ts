1ae477df4cea0f1b7d464723fedaab42d074a80d
Daniel Jones <daniel.jones@zookal.com>
2021-01-21 15:50:48 +1100

    Whitelisting of request properties

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true /* Enabling "whitelist" feature of ValidationPipe */,
+      forbidNonWhitelisted: true /* Throw errors when whitelisted properties are found */,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
