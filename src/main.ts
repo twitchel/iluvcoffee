25fbd451e927279aa6a6c37fa9dbbd92a7c6882b
Daniel Jones <daniel.jones@zookal.com>
2021-01-21 16:00:36 +1100

    Enable auto transformation

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
     new ValidationPipe({
       whitelist: true /* Enabling "whitelist" feature of ValidationPipe */,
       forbidNonWhitelisted: true /* Throw errors when whitelisted properties are found */,
+      transform: true /* Enabling auto transform feature of ValidationPipe */,
     }),
   );
   await app.listen(3000);
